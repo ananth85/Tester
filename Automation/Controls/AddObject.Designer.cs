ca1a27c96f790f7db3c61aff545f4cfea18cab4a
ananth85 <a85ram@gmail.com>
2016-04-04 16:38:20 +1000

    New user control and not the source code

--- Automation/Controls/AddObject.Designer.cs
+++ Automation/Controls/AddObject.Designer.cs
@@ -28,56 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.T_ObjectName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.T_FindBy = new System.Windows.Forms.TextBox();
+            this.T_Property = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.T_FrameName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Screen Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 70);
+            this.label2.Location = new System.Drawing.Point(20, 39);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Object Name";
             // 
-            // label3
+            // T_ObjectName
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 14);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Screen Name";
+            this.T_ObjectName.Location = new System.Drawing.Point(126, 39);
+            this.T_ObjectName.Name = "T_ObjectName";
+            this.T_ObjectName.Size = new System.Drawing.Size(141, 20);
+            this.T_ObjectName.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Find by";
+            // 
+            // T_FindBy
+            // 
+            this.T_FindBy.Location = new System.Drawing.Point(126, 66);
+            this.T_FindBy.Name = "T_FindBy";
+            this.T_FindBy.Size = new System.Drawing.Size(141, 20);
+            this.T_FindBy.TabIndex = 7;
+            // 
+            // T_Property
+            // 
+            this.T_Property.Location = new System.Drawing.Point(126, 92);
+            this.T_Property.Name = "T_Property";
+            this.T_Property.Size = new System.Drawing.Size(141, 20);
+            this.T_Property.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Property";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(126, 118);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(141, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Description";
+            // 
+            // T_FrameName
+            // 
+            this.T_FrameName.Location = new System.Drawing.Point(126, 13);
+            this.T_FrameName.Name = "T_FrameName";
+            this.T_FrameName.Size = new System.Drawing.Size(141, 20);
+            this.T_FrameName.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Frame";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // AddObject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label3);
+            this.AutoSize = true;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.T_FrameName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.T_Property);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.T_FindBy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.T_ObjectName);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "AddObject";
-            this.Size = new System.Drawing.Size(282, 124);
+            this.Size = new System.Drawing.Size(282, 181);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox T_ObjectName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox T_FindBy;
+        private System.Windows.Forms.TextBox T_Property;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox T_FrameName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
     }
 }
---
+++ Automation/Controls/AddScreen.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Automation.Controls
+{
+    partial class AddScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.T_ScreenName = new System.Windows.Forms.TextBox();
+            this.T_ScreenID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // T_ScreenName
+            // 
+            this.T_ScreenName.Location = new System.Drawing.Point(120, 44);
+            this.T_ScreenName.Name = "T_ScreenName";
+            this.T_ScreenName.Size = new System.Drawing.Size(141, 20);
+            this.T_ScreenName.TabIndex = 16;
+            // 
+            // T_ScreenID
+            // 
+            this.T_ScreenID.Enabled = false;
+            this.T_ScreenID.Location = new System.Drawing.Point(120, 14);
+            this.T_ScreenID.Name = "T_ScreenID";
+            this.T_ScreenID.Size = new System.Drawing.Size(61, 20);
+            this.T_ScreenID.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Screen ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Screen Name";
+            // 
+            // AddScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.T_ScreenName);
+            this.Controls.Add(this.T_ScreenID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "AddScreen";
+            this.Size = new System.Drawing.Size(268, 98);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox T_ScreenName;
+        private System.Windows.Forms.TextBox T_ScreenID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
